8f3d3767d916c0ddda9e900f5f474cc4036a51e6
Joan Salazar <105525773+Joangeek@users.noreply.github.com>
2023-05-07 17:46:00 -0500

    firebase_storage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'storage-f5ff1',
+    appId: '1:181133634264:web:96b5a2b89b7de0a76c05ad',
+    storageBucket: 'storage-f5ff1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCJ2zcVzBWPL9jo_5ufGeQEKyVIyDI2dDs',
+    authDomain: 'storage-f5ff1.firebaseapp.com',
+    messagingSenderId: '181133634264',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'storage-f5ff1',
+    appId: '1:181133634264:web:96b5a2b89b7de0a76c05ad',
+    storageBucket: 'storage-f5ff1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCJ2zcVzBWPL9jo_5ufGeQEKyVIyDI2dDs',
+    authDomain: 'storage-f5ff1.firebaseapp.com',
+    messagingSenderId: '181133634264',
+  },
   production: false
 };
 
